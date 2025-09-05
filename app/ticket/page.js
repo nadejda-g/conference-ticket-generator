c0e02f00f7066f2dcd54502db813599a8d1a0db3
nadejda-g <ndgolovaciuc@gmail.com>
2025-09-05 18:01:43 +0300

    add desktop final design

--- app/ticket/page.js
+++ app/ticket/page.js
@@ -8,8 +8,8 @@ export default function TicketPage() {
         <div className={styles.mainPage}>
             <Logo/>
             <div className={styles.header}>
-                <p className={styles.mainHeader}>Congrats, Jonatan Kristof! Your ticket is ready.</p>
-                <p className={styles.subHeader}>We've emailed your ticket to jonatan@email.com and will send updates in the run up to the event.</p>
+                <p className={styles.mainHeader}>Congrats, <span className={styles.nameOrange}>Jonatan Kristof</span>! Your ticket is ready.</p>
+                <p className={styles.subHeader}>We've emailed your ticket to<br/><span className={styles.emailOrange}>jonatan@email.com</span> and will send updates in<br/>the run up to the event.</p>
             </div>
             <Ticket/>
         </div>
